a6ca4be19c2088c6abeb949ab47d66062af5492d
joshflores <Josh_flores@protonmail.com>
2024-02-25 18:44:19 -0600

    Dev Addition

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          New Dev React site
         </a>
       </header>
     </div>
